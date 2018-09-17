aae18a354701c09d966e9defc3816a05bc48e4a6
joseoyoung <josy3465@naver.com>
2018-09-17 13:51:44 +0900

    소스 첫 임포팅

---
+++ main.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var fs = require('fs');
+var app = http.createServer(function(request,response){
+    var url = request.url;
+    if(request.url == '/'){
+      url = '/index.html';
+    }
+    if(request.url == '/favicon.ico'){
+      return response.writeHead(404);
+      response.end();
+      return;
+    }
+    response.writeHead(200);
+    response.end(fs.readFileSync(__dirname + url));
+    //response.writeHead(200);
+    //console.log(__dirname + url);
+    //response.end('egoing : '+url);
+
+});
+console.log("실행 포트 번호는 :"+process.env.PORT);
+app.listen(process.env.PORT || 3000);
